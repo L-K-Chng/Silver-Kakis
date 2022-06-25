34813bfe7593e2f4cdd35ae3d48b96fbf674101c
CJun1039 <e0726202@u.nus.edu>
2022-06-25 11:12:46 +0800

    Added sign out option.

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import './login_screen.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -8,10 +10,33 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  _signOut() async {
+    await _firebaseAuth.signOut().then((value) => Navigator.of(context)
+        .pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => LoginScreen()),
+            (route) => false));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Profile Page!',
-      style: TextStyle(fontSize: 50),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile Page'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              _signOut();
+            },
+            tooltip: 'Log Out',
+            icon: const Icon(
+              Icons.logout_rounded,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
